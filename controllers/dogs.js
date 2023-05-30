fa9c2412401f35689b0e39f18c6590b863d471e3
willdoliver <willdoliverml@gmail.com>
2023-05-30 11:48:50 -0300

    Dogs APIs working using mongoose ORM

--- controllers/dogs.js
+++ controllers/dogs.js
@@ -2,31 +2,31 @@ const Dogs = require('../models/dogs')
 const asyncWrapper = require('../middleware/async')
 
 const getAllDogs = asyncWrapper( async (req,res) => {
-    console.log("Called getAllDogs")
-    // const dogs = await Dogs.find({}) ToDo
-    res.status(200).json({ success: true })
+    const dogs = await Dogs.find({}).select('name race age')
+    res.status(200).json({ success: true, data: dogs })
 })
 
 const getDog = asyncWrapper( async (req,res) => {
-    const { id: dogId } = req.body
-    console.log(`Called getDog, id: ${dogId}`)
-    res.status(200).json({ success: true })
-
+    const { id: dogId } = req.params
+    const dog = await Dogs.findById(dogId)
+    res.status(200).json({ success: true, data: dog })
 })
 
 const createDog = asyncWrapper( async (req,res) => {
-    console.log(`Called createDog, body: ${req.body}`)
-    res.status(200).json({ success: true, data: req.body })
+    const dog = await Dogs.create(req.body)
+    res.status(200).json({ success: true, data: dog })
 })
+
 const updateDog = asyncWrapper( async (req,res) => {
-    const { id: dogId } = req.body
-    console.log(`Called updateDog, id: ${dogId}, body: ${req.body}`)
-    res.status(200).json({ success: true, data: req.body })
+    const { id: dogId } = req.params
+    const updated = await Dogs.findByIdAndUpdate(dogId,req.body, {new: true} )
+    res.status(200).json({ success: true, data: updated })
 })
 
 const deleteDog = asyncWrapper( async (req,res) => {
-    const { id: dogId } = req.body
-    console.log(`Called deleteDog, id: ${dogId}`)
+    const { id: dogId } = req.params
+    await Dogs.findByIdAndDelete(dogId)
+    res.status(200).json({ success: true })
 })
 
 module.exports = {
